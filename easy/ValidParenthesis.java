6733f6809139eba9df64eec5bea150221c3da194
Rohan Yadav <30825787+vrrohan@users.noreply.github.com>
2025-02-14 13:19:20 +0530

    Create ValidParenthesis.java
    check if paranthesis is balancing or not

---
+++ easy/ValidParenthesis.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+/*
+ * Problem link - https://leetcode.com/problems/valid-parentheses/description/
+ */
+public class ValidParenthesis {
+
+    /*
+     * tc - O(n)
+     * sc - O(n)
+     */
+    public boolean isValid(String s) {
+        if(s.length()==1) return false;
+		else {
+			boolean isValidParanthesis = true;
+			Stack<Character> st = new Stack<Character>();
+			for(int i=0; i<s.length(); i++) {
+				if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='[') {
+					st.push(s.charAt(i));
+				} else if(s.charAt(i)==')' && !st.isEmpty() && st.peek()=='(') {
+					st.pop();
+				} else if(s.charAt(i)=='}' && !st.isEmpty() && st.peek()=='{') {
+					st.pop();
+				} else if(s.charAt(i)==']' && !st.isEmpty() && st.peek()=='[') {
+					st.pop();
+				} else {
+					isValidParanthesis = false;
+					break;
+				}
+			}
+			return (st.size()==0 && isValidParanthesis) ? isValidParanthesis : false;
+		}
+}

3421456e4bdbdb6e019f18ea03d90c4fc6d13ca3
Rohan Yadav <30825787+vrrohan@users.noreply.github.com>
2025-02-14 13:04:06 +0530

    Create MaximumAverageSubArray.java
    maximum average subarray

---
+++ easy/MaximumAverageSubArray.java
@@ -0,0 +1,30 @@
+public class MaximumAverageSubArray {
+    /*
+     * Problem link -
+     * https://leetcode.com/problems/maximum-average-subarray-i/description/
+     * tc - O(n)
+     * sc - O(1)
+     */
+    public double findMaxAverage(int[] nums, int k) {
+        double maxAvg = 0.0;
+        int sum = 0;
+
+        // calculate sum of first k elements
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        maxAvg = (double) sum / k;
+
+        for (int i = 1; i <= nums.length - k; i++) {
+            double currAvg = 0.0;
+            // first subtract the previous element which was removed from window
+            sum -= nums[i - 1];
+            // then add the new element which was added to window
+            sum += nums[i + k - 1];
+            // now calculate the new average and then compare it will maxAvg
+            currAvg = (double) sum / k;
+            maxAvg = Math.max(maxAvg, currAvg);
+        }
+        return maxAvg;
+    }
+}
